fb169a76076afd1047fe8990fc779caf21f77f1d
Sina Huster <sina@huster.me.uk>
2025-10-06 15:04:44 +0200

    Add exercise 2351

---
+++ Easy/letter_twice.py
@@ -0,0 +1,24 @@
+""" 
+Given a string s consisting of lowercase English letters, return the first letter to appear twice.
+
+Note:
+
+A letter a appears twice before another letter b if the second occurrence of a is before the second occurrence of b.
+s will contain at least one letter that appears twice.
+
+Constraints:
+2 <= s.length <= 100
+s consists of lowercase English letters.
+s has at least one repeated letter.
+"""
+
+class Solution:
+    def repeatedCharacter(self, s: str) -> str:
+        seen = {}
+
+        for c in s:
+            if c in seen:
+                return c
+            seen[c] = c
+        
+        return " "
