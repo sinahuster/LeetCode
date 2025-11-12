df6406a400423bad35cfb7a746c72a7bf037df44
Sina Huster <sina@huster.me.uk>
2025-11-12 13:21:24 +0100

    Add exercise 1448

---
+++ Medium/good_nodes_binary_tree.py
@@ -0,0 +1,33 @@
+"""
+Given a binary tree root, a node X in the tree is named good if in the path from root to X there are no nodes with a value greater than X.
+
+Return the number of good nodes in the binary tree.
+
+Constraints:
+The number of nodes in the binary tree is in the range [1, 10^5].
+Each node's value is between [-10^4, 10^4].
+"""
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def goodNodes(self, root: TreeNode) -> int:
+        def dfs(node, max_so_far):
+            if not node:
+                return 0
+
+            left = dfs(node.left, max(max_so_far, node.val))
+            right = dfs(node.right, max(max_so_far, node.val))
+            ans = left + right
+            if node.val >= max_so_far:
+                ans += 1
+
+            return ans
+
+        return dfs(root, float("-inf"))
+    
+# This has time and space complexity O(n)
