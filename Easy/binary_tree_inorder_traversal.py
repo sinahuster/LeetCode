de7ea793706520574d6cc8d769a2185123870686
Sina Huster <sina@huster.me.uk>
2025-11-28 09:33:10 +0100

    Add exercise 94

---
+++ Easy/binary_tree_inorder_traversal.py
@@ -0,0 +1,26 @@
+"""
+Given the root of a binary tree, return the inorder traversal of its nodes' values.
+
+Constraints:
+The number of nodes in the tree is in the range [0, 100].
+-100 <= Node.val <= 100
+"""
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        if root == None:
+            return []
+
+        left = self.inorderTraversal(root.left)
+        right = self.inorderTraversal(root.right)
+
+        return left + [root.val] + right
+    
+
+# This has time and space complexity O(n) 
