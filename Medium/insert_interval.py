120c780e02e6d1c2ca660d529db6b1edc31ca113
Sina Huster <sina@huster.me.uk>
2026-01-07 13:09:52 +0100

    Add exercise 57

---
+++ Medium/insert_interval.py
@@ -0,0 +1,44 @@
+"""
+You are given an array of non-overlapping intervals intervals where intervals[i] = [starti, endi] represent the start and the end of the ith interval and intervals is sorted in ascending order by starti. 
+You are also given an interval newInterval = [start, end] that represents the start and end of another interval.
+
+Insert newInterval into intervals such that intervals is still sorted in ascending order by starti and intervals still does not have any overlapping intervals (merge overlapping intervals if necessary).
+
+Return intervals after the insertion.
+
+Note that you don't need to modify intervals in-place. You can make a new array and return it.
+
+Constraints:
+0 <= intervals.length <= 10^4
+intervals[i].length == 2
+0 <= starti <= endi <= 10^5
+intervals is sorted by starti in ascending order.
+newInterval.length == 2
+0 <= start <= end <= 10^5
+"""
+
+class Solution:
+    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+
+        result = []
+        n = len(intervals)
+        i = 0
+
+        while i < n and intervals[i][1] < newInterval[0]:
+            result.append(intervals[i])
+            i += 1
+
+        while i < n and intervals[i][0] <= newInterval[1]:
+            newInterval[0] = min(newInterval[0], intervals[i][0])
+            newInterval[1] = max(newInterval[1], intervals[i][1])
+            i += 1
+
+        result.append(newInterval)
+
+        while i < n:
+            result.append(intervals[i])
+            i += 1
+
+        return result
+    
+# This has time and space complexity O(n) as we need to look at each interval and store at most n + 1 intervals in the new array
