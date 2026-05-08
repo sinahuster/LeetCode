dcabc41188fddd3f6aa808a5dfbe676753098407
Sina Huster <sina@huster.me.uk>
2026-05-08 08:11:15 +0200

    Add exercise 881

---
+++ Medium/boats_to_save_people.py
@@ -0,0 +1,31 @@
+"""
+You are given an array people where people[i] is the weight of the ith person, and an infinite number of boats where each boat can carry a maximum weight of limit. 
+Each boat carries at most two people at the same time, provided the sum of the weight of those people is at most limit.
+
+Return the minimum number of boats to carry every given person.
+
+Constraints:
+1 <= people.length <= 5 * 10^4
+1 <= people[i] <= limit <= 3 * 10^4
+"""
+
+class Solution:
+    def numRescueBoats(self, people: List[int], limit: int) -> int:
+        
+        people.sort()
+
+        h = len(people) - 1
+        l = 0 
+        boats = 0
+
+        while h >= l:
+            if people[h] + people[l] <= limit:
+                l += 1
+            h -= 1
+            boats += 1
+
+        return boats
+                
+
+# This has time complexity O(n log n), where n is the length of people,
+# and space complexity O(1)
