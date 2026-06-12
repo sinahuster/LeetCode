17736a8373015b57d68564fbe2bf6a1c74f967ac
Sina Huster <sina@huster.me.uk>
2026-06-12 12:13:42 +0200

    Add exercise 704

---
+++ Easy/binary_search.py
@@ -0,0 +1,33 @@
+"""
+Given an array of integers nums which is sorted in ascending order, and an integer target, write a function to search target in nums. 
+If target exists, then return its index. Otherwise, return -1.
+
+You must write an algorithm with O(log n) runtime complexity.
+
+Constraints:
+1 <= nums.length <= 10^4
+-10^4 < nums[i], target < 10^4
+All the integers in nums are unique.
+nums is sorted in ascending order.
+"""
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        
+        left = 0
+        right = len(nums) - 1
+
+        while left <= right:
+            mid = (left + right) // 2
+            print(mid)
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                left = mid + 1
+            else:
+                right = mid - 1
+
+        return -1
+    
+# This has time complexity O(log n), where n is the size of nums,
+# and space complexity O(1)
